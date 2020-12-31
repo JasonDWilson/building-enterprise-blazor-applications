89ddc8220207f32d995257bd56250e7faf66ef91
Jason Wilson <jason.wilson@flashparking.com>
2020-12-31 10:27:14 -0600

    Broke TaskList into seperate component

---
+++ BethanysPieShopHRM.UI/Pages/TaskList.razor.cs
@@ -0,0 +1,34 @@
+﻿using BethanysPieShopHRM.Shared;
+using BethanysPieShopHRM.UI.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.UI.Pages
+{
+    public partial class TaskList
+    {
+        [Parameter]
+        public int Count { get; set; }
+
+        [Inject]
+        public NavigationManager navManager { get; set; }
+
+        public List<HRTask> Tasks { get; set; } = new List<HRTask>();
+
+        [Inject]
+        public ITaskDataService taskService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var tasks = (await taskService.GetAllTasks()).ToList();
+            if (Count != 0)
+                tasks = tasks.Take(Count).ToList();
+            Tasks = tasks;
+        }
+
+        public void AddTask() { navManager.NavigateTo("taskedit"); }
+    }
+}
